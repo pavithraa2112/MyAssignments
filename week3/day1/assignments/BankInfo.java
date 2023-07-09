b1346b3110112160751ff7c84463c5d60b52a6e4
pavithraa2112 <137678244+pavithraa2112@users.noreply.github.com>
2023-07-09 13:05:10 +0530

    Add files via upload

---
+++ week3/day1/assignments/BankInfo.java
@@ -0,0 +1,18 @@
+package week3.day1.assignments;
+
+public class BankInfo {
+
+	public void saving() {
+		System.out.println("Saving() from BankInfo Class");
+
+	}
+	
+	public void fixed() {
+		System.out.println("Fixed() from BankInfo Class");
+
+	}
+	public void deposit() {
+		System.out.println("deposit() from BankInfo Class");
+
+	}
+}
